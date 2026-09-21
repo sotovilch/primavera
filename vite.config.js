75c8bda538781c002f27d0cb4662044df2483c3b
Gerardo Soto <gersoto3110.developer@gmail.com>
2026-09-21 19:56:34 -0300

    feat: config para github pages y actions

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Rutas relativas para que funcione tanto en la raíz como en subdirectorios de GitHub Pages
+  base: './',
+});
